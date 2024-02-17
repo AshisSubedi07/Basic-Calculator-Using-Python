3a8a95fd5689cb111ea31ab067bb0901cc8481b8
Ashis Subedi <73783936+AshisSubedi07@users.noreply.github.com>
2024-02-17 04:10:11 -0800

    Add files via upload

---
+++ day7calculator.py
@@ -0,0 +1,8 @@
+#A simple python program which can do all the arithmetic opecation on real numbers.
+a=input("Enter first number")
+b=input("Enter second number")
+print("The addition is: ",int(a)+int(b))  #addition
+print("The addition is: ",int(a)-int(b))  #Substraction
+print("The addition is: ",int(a)*int(b))  #multiplication
+print("The addition is: ",int(a)/int(b))  #Division
+print("The addition is: ",int(a)%int(b))  #Modulas
